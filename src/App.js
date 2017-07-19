b058abe01daacec1cdfd9fd07ad5eb1192c31409
amansawhney <amansawhney@sawhneygames.com>
2017-07-19 09:51:27 -0400

    added Bootstrap and ladder logo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Metric from './components/Metric';
 import './App.css';
+import logo from './logo-on-light.svg';
 
 class App extends Component {
   constructor(props) {
@@ -30,33 +31,38 @@ class App extends Component {
 
   render() {
     return (
-      <div className="search">
-        <header>
-          <input
-            onChange={this.handleSearchTermChange}
-            value={this.state.searchTerm}
-            type="text"
-          />
-        </header>
-        <div>
-          {this.state.metricData.metrics
-            .filter(
-              metric =>
-                `${metric.industry} ${metric.metricName} ${metric.platform}`
-                  .toUpperCase()
-                  .indexOf(this.state.searchTerm.toUpperCase()) >= 0,
-            )
-            .map(metric =>
-              <div>
-                <table>
-                  <Metric
-                    industry={metric.industry}
-                    name={metric.metricName}
-                    platform={metric.platform}
-                  />
-                </table>
-              </div>,
-            )}
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-8 col-sm-offset-2">
+            <header>
+              <img src={logo} className="App-logo" alt="logo" />
+              <input className="col-sm-12"
+                onChange={this.handleSearchTermChange}
+                value={this.state.searchTerm}
+                type="text"
+              />
+            </header>
+            <div>
+              {this.state.metricData.metrics
+                .filter(
+                  metric =>
+                    `${metric.industry} ${metric.metricName} ${metric.platform}`
+                      .toUpperCase()
+                      .indexOf(this.state.searchTerm.toUpperCase()) >= 0,
+                )
+                .map(metric =>
+                  <div>
+                    <table>
+                      <Metric
+                        industry={metric.industry}
+                        name={metric.metricName}
+                        platform={metric.platform}
+                      />
+                    </table>
+                  </div>,
+                )}
+            </div>
+          </div>
         </div>
       </div>
     );
